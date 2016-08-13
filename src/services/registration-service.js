6585d178c676df630dc022aab5d7ce6fe2912997
Divan Visagie <visagiedivan@gmail.com>
2016-08-13 21:27:44 +0200

    no duplicate users

--- src/services/registration-service.js
+++ src/services/registration-service.js
@@ -4,13 +4,25 @@ const pw = credential()
 function RegistrationService (userRepository) {
   return {
     registerUser (user, callback) {
+      if (!user.username) {
+        return callback('username not found')
+      }
       if (!user.password) {
         return callback('no password in request')
       }
-      pw.hash(user.password, function (err, hash) {
-        if (err) { throw err }
-        user.password = hash
-        userRepository.createUser(user, callback)
+      userRepository.findUserByUsername(user.username, (err, data) => {
+        if (err) {
+          return callback(err)
+        }
+        if (data.length === 0) {
+          pw.hash(user.password, function (err, hash) {
+            if (err) { throw err }
+            user.password = hash
+            userRepository.createUser(user, callback)
+          })
+        } else {
+          callback('user already exists')
+        }
       })
     }
   }
